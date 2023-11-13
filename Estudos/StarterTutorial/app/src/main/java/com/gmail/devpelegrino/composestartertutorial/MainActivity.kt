6344a570e3defa2883359e43bdca042de2048aec
Felipe Pelegrino <devpelegrino@gmail.com>
2023-11-13 20:14:22 -0300

    add image

--- Estudos/StarterTutorial/app/src/main/java/com/gmail/devpelegrino/composestartertutorial/MainActivity.kt
+++ Estudos/StarterTutorial/app/src/main/java/com/gmail/devpelegrino/composestartertutorial/MainActivity.kt
@@ -3,9 +3,12 @@ package com.gmail.devpelegrino.composestartertutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -27,9 +30,15 @@ data class Message(val author: String, val body: String)
 // Por convenção o padrão Composable começa com PascalCase
 @Composable
 fun MessageCard(message: Message) {
-    Column {
-        Text(text = message.author)
-        Text(text = message.body)
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Profile Photo"
+        )
+        Column {
+            Text(text = message.author)
+            Text(text = message.body)
+        }
     }
 }
 
