0d775589182a044b84059ec987c0bf5f34c304da
Felipe Pelegrino <devpelegrino@gmail.com>
2023-11-13 20:19:09 -0300

    add estilizacao aos elementos

--- Estudos/StarterTutorial/app/src/main/java/com/gmail/devpelegrino/composestartertutorial/MainActivity.kt
+++ Estudos/StarterTutorial/app/src/main/java/com/gmail/devpelegrino/composestartertutorial/MainActivity.kt
@@ -6,10 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,13 +39,22 @@ data class Message(val author: String, val body: String)
 // Por convenção o padrão Composable começa com PascalCase
 @Composable
 fun MessageCard(message: Message) {
-    Row {
+    Row(Modifier.padding(8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.profile_picture),
-            contentDescription = "Profile Photo"
+            contentDescription = "Profile Photo",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
+
+        //Spacer horizontal
+        Spacer(modifier = Modifier.width(8.dp))
+
         Column {
             Text(text = message.author)
+            // Spacer vertical
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = message.body)
         }
     }
