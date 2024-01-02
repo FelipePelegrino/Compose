f834bc5cd06b1933c71a14be9aff774b063bb3ff
Felipe Pelegrino <devpelegrino@gmail.com>
2024-01-02 20:57:31 -0300

    add state | remember explanation

--- Estudos/BasicCodelab/app/src/main/java/com/gmail/devpelegrino/MainActivity.kt
+++ Estudos/BasicCodelab/app/src/main/java/com/gmail/devpelegrino/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,8 +47,17 @@ fun MyApp(
     }
 }
 
+/*
+* Diferentes objetos Greeting em tela, são UI diferentes, que contem valores unicos em suas
+* variáveis de estados.
+* */
 @Composable
 private fun Greeting(name: String, modifier: Modifier = Modifier) {
+    // internal state  = private variable
+    // Precisamos do remember, para guardar a informação durante a recomposition
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -56,16 +67,18 @@ private fun Greeting(name: String, modifier: Modifier = Modifier) {
         * até o necessário para eles serem exibidos.
         * */
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
                 Text(text = "Hello ")
                 Text(text = name)
             }
             ElevatedButton(
                 onClick = {
-
+                    expanded.value = !expanded.value
                 }
             ) {
-                Text("Show more")
+                if (expanded.value) Text("Show less") else Text("Show more")
             }
         }
     }
