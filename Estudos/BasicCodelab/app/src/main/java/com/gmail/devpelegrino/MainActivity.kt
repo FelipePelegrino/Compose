f779baa4dfce85ddf220bc83a656d391c2250fd0
Felipe Pelegrino <devpelegrino@gmail.com>
2024-01-03 21:38:01 -0300

    add lazy column

--- Estudos/BasicCodelab/app/src/main/java/com/gmail/devpelegrino/MainActivity.kt
+++ Estudos/BasicCodelab/app/src/main/java/com/gmail/devpelegrino/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -119,12 +121,12 @@ fun OnboardingScreen(
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
-        }
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+       items(items = names) {
+           Greeting(name = it)
+       }
     }
 }
 
