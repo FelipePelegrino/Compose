3ec95d910ea9fc993790d15219e1868167dc854c
Felipe Pelegrino <devpelegrino@gmail.com>
2024-01-11 22:17:47 -0300

    basic layout to continue codelab

---
+++ Estudos/BasicStateCodelab/app/src/main/java/com/gmail/devpelegrino/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,16 @@
+package com.gmail.devpelegrino.basicstatecodelab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WellnessScreenPreview() {
+    WellnessScreen()
+}
