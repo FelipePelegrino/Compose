dd6f44fe1e026284c2afcba5d582d772508ec913
Felipe Pelegrino <devpelegrino@gmail.com>
2024-01-17 22:19:51 -0300

    voltando para o código antes do exemplo para entender o comportamento de remember

--- Estudos/BasicStateCodelab/app/src/main/java/com/gmail/devpelegrino/basicstatecodelab/WaterCounter.kt
+++ Estudos/BasicStateCodelab/app/src/main/java/com/gmail/devpelegrino/basicstatecodelab/WaterCounter.kt
@@ -1,7 +1,6 @@
 package com.gmail.devpelegrino.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -16,29 +15,13 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count by remember { mutableStateOf(0) }
-
     Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { showTask = false },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
-            Text(text = "You've had $count glasses.")
+            Text("You've had $count glasses.")
         }
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(onClick = { count++ }, enabled = count < 10) {
-                Text("Add one")
-            }
-            Button(
-                onClick = { count = 0 },
-                Modifier.padding(start = 8.dp)
-            ) {
-                Text("Clear water count")
-            }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
