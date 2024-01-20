b0e6f7da736e9bad244b450bdf00c4095083a12c
Felipe Pelegrino <devpelegrino@gmail.com>
2024-01-20 19:26:00 -0300

    implementacao correta do click de excluir item

--- Estudos/BasicStateCodelab/app/src/main/java/com/gmail/devpelegrino/basicstatecodelab/WellnessScreen.kt
+++ Estudos/BasicStateCodelab/app/src/main/java/com/gmail/devpelegrino/basicstatecodelab/WellnessScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -18,7 +20,15 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
             onCountChange = { count++ },
             modifier = modifier
         )
-        WellnessTasksList()
+
+////         Don't do this!
+//
+//        val list = remember { mutableStateListOf<WellnessTask>() }
+//
+//        list.addAll(getWellnessTasks())
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
     }
 }
 
@@ -26,4 +36,11 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WellnessScreenPreview() {
     WellnessScreen()
+}
+
+private fun getWellnessTasks() = List(30) { i ->
+    WellnessTask(
+        id = i,
+        label = "Task # $i"
+    )
 }
--- Estudos/BasicStateCodelab/app/src/main/java/com/gmail/devpelegrino/basicstatecodelab/WellnessTaskItem.kt
+++ Estudos/BasicStateCodelab/app/src/main/java/com/gmail/devpelegrino/basicstatecodelab/WellnessTaskItem.kt
@@ -11,71 +11,53 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WellnessTaskItem(
     taskName: String,
     checked: Boolean,
-    isClosed: Boolean,
     onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    if (!isClosed) {
-        Row(
-            modifier = modifier,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(start = 16.dp),
-                text = taskName
-            )
-            Checkbox(
-                checked = checked,
-                onCheckedChange = onCheckedChange
-            )
-            IconButton(onClick = onClose) {
-                Icon(Icons.Filled.Close, contentDescription = "Close")
-            }
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
         }
     }
 }
 
 @Composable
-fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     var checkedState by rememberSaveable { mutableStateOf(false) }
-    var isClosed by rememberSaveable { mutableStateOf(false) }
 
     WellnessTaskItem(
         taskName = taskName,
         checked = checkedState,
-        isClosed = isClosed,
         onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = { isClosed = true },
+        onClose = onClose,
         modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun WellnessTaskItemPreview() {
-    var isChecked by remember { mutableStateOf(false) }
-    var isClosed by rememberSaveable { mutableStateOf(false) }
-
-    WellnessTaskItem(
-        taskName = "Task #1",
-        checked = isChecked,
-        isClosed = isClosed,
-        onCheckedChange = { isChecked = it },
-        onClose = { isClosed = true }
     )
 }
