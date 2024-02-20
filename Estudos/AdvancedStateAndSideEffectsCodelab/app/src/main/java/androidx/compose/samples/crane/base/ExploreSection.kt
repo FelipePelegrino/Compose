41cf38b07d3726f5a6ff656e4e2466826e9f61ec
Felipe Pelegrino <devpelegrino@gmail.com>
2024-02-20 13:06:57 -0300

    derivedStateOf e produceState

--- Estudos/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/ExploreSection.kt
+++ Estudos/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/ExploreSection.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -36,12 +37,32 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.samples.crane.R
+import androidx.compose.samples.crane.data.BALI
+import androidx.compose.samples.crane.data.BARCELONA
+import androidx.compose.samples.crane.data.BIGSUR
+import androidx.compose.samples.crane.data.CORDOBA
+import androidx.compose.samples.crane.data.CRETE
+import androidx.compose.samples.crane.data.DALLAS
 import androidx.compose.samples.crane.data.ExploreModel
+import androidx.compose.samples.crane.data.GRANADA
+import androidx.compose.samples.crane.data.KHUMBUVALLEY
+import androidx.compose.samples.crane.data.LONDON
+import androidx.compose.samples.crane.data.MADRID
+import androidx.compose.samples.crane.data.MALDIVAS
+import androidx.compose.samples.crane.data.NAPLES
+import androidx.compose.samples.crane.data.PARIS
+import androidx.compose.samples.crane.data.ROME
+import androidx.compose.samples.crane.data.WASHINGTONDC
 import androidx.compose.samples.crane.home.OnExploreItemClicked
 import androidx.compose.samples.crane.ui.BottomSheetShape
 import androidx.compose.samples.crane.ui.crane_caption
@@ -52,10 +73,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest.Builder
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -72,10 +95,39 @@ fun ExploreSection(
                 style = MaterialTheme.typography.caption.copy(color = crane_caption)
             )
             Spacer(Modifier.height(8.dp))
-            // TODO Codelab: derivedStateOf step
-            // TODO: Show "Scroll to top" button when the first item of the list is not visible
-            val listState = rememberLazyListState()
-            ExploreList(exploreList, onItemClicked, listState = listState)
+            Box(Modifier.weight(1f)) {
+                val listState = rememberLazyListState()
+                ExploreList(exploreList, onItemClicked, listState = listState)
+
+                /*
+                * Cria um novo state a partir do state de outro composable.
+                * Porém, o calculo para SABER o novo state é realizado toda recomposição em que o state
+                * interno é alterado, mas a recomposição, do novo valor gerado, só acontece se o
+                * novo valor for diferente do ultimo gerado.
+                * */
+                val showButton by remember {
+                    derivedStateOf {
+                        listState.firstVisibleItemIndex > 0
+                    }
+                }
+                if (showButton) {
+                    val coroutineScope = rememberCoroutineScope()
+                    FloatingActionButton(
+                        backgroundColor = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .navigationBarsPadding()
+                            .padding(bottom = 8.dp),
+                        onClick = {
+                            coroutineScope.launch {
+                                listState.scrollToItem(0)
+                            }
+                        }
+                    ) {
+                        Text("Up!")
+                    }
+                }
+            }
         }
     }
 }
@@ -162,4 +214,90 @@ private fun ExploreImageContainer(content: @Composable () -> Unit) {
     Surface(Modifier.size(width = 60.dp, height = 60.dp), RoundedCornerShape(4.dp)) {
         content()
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ExploreSectionPreview() {
+    ExploreSection(
+        title = "Explore Flights by Destination",
+        exploreList = listOf(
+            ExploreModel(
+                city = KHUMBUVALLEY,
+                description = "Nonstop - 5h 16m+",
+                imageUrl = "https://images.unsplash.com/photo-1544735716-392fe2489ffa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = MADRID,
+                description = "Nonstop - 2h 12m+",
+                imageUrl = "https://images.unsplash.com/photo-1539037116277-4db20889f2d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = BALI,
+                description = "Nonstop - 6h 20m+",
+                imageUrl = "https://images.unsplash.com/photo-1518548419970-58e3b4079ab2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = ROME,
+                description = "Nonstop - 2h 38m+",
+                imageUrl = "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = GRANADA,
+                description = "Nonstop - 2h 12m+",
+                imageUrl = "https://images.unsplash.com/photo-1534423839368-1796a4dd1845?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = MALDIVAS,
+                description = "Nonstop - 9h 24m+",
+                imageUrl = "https://images.unsplash.com/photo-1544550581-5f7ceaf7f992?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = WASHINGTONDC,
+                description = "Nonstop - 7h 30m+",
+                imageUrl = "https://images.unsplash.com/photo-1557160854-e1e89fdd3286?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = BARCELONA,
+                description = "Nonstop - 2h 12m+",
+                imageUrl = "https://images.unsplash.com/photo-1562883676-8c7feb83f09b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = CRETE,
+                description = "Nonstop - 1h 50m+",
+                imageUrl = "https://images.unsplash.com/photo-1486575008575-27670acb58db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = NAPLES,
+                description = "Nonstop - 1h 45m+",
+                imageUrl = "https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixlib=rb-1.2.1&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = DALLAS,
+                description = "Nonstop - 8h 30m+",
+                imageUrl = "https://images.unsplash.com/photo-1495749388945-9d6e4e5b67b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = CORDOBA,
+                description = "1 stop - 11h 30m+",
+                imageUrl = "https://images.unsplash.com/photo-1562625964-ffe9b2f617fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250&q=250"
+            ),
+            ExploreModel(
+                city = BIGSUR,
+                description = "Nonstop - 10h 45m+",
+                imageUrl = "https://images.unsplash.com/photo-1561409037-c7be81613c1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = LONDON,
+                description = "Nonstop - 1h 5m+",
+                imageUrl = "https://images.unsplash.com/photo-1505761671935-60b3a7427bad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250"
+            ),
+            ExploreModel(
+                city = PARIS,
+                description = "Nonstop - 2h 25m+",
+                imageUrl = "https://images.unsplash.com/photo-1509299349698-dd22323b5963?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250"
+            )
+        ),
+        onItemClicked = { }
+    )
 }
