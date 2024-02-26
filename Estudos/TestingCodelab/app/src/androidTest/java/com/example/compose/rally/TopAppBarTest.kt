bedd8b1e46b84cfbfd1df74a128583a0f28a6d7a
Felipe Pelegrino <devpelegrino@gmail.com>
2024-02-26 16:36:33 -0300

    pq as vezes utilizar onNodeWithText pode falhar

--- Estudos/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ Estudos/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -3,6 +3,8 @@ package com.example.compose.rally
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Rule
@@ -38,6 +40,32 @@ class TopAppBarTest {
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
             .assertIsSelected()
+    }
+
+    /*
+    * Não funciona procurar por texto nesse caso pois o composable setou via clearAndSetSemantics
+    * */
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        /*
+        * Não tem ferramenta ainda para semantic tree (26/02/2024) então
+        * iremos na raiz, utilizar a fun printToLog para ver a semantic tree gerada.
+        * */
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+
+        composeTestRule
+//            .onNodeWithText(RallyScreen.Accounts.name.uppercase())
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists() // Still fails
     }
 
 }
